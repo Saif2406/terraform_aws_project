f2948a5d4c404e82e4585c144e3684f4bc935ae1
SAIF KHAN AHMED <55284450+Saif2406@users.noreply.github.com>
2023-11-29 19:31:05 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
+# Create a VPC 
 resource "aws_vpc" "myvpc" {
     cidr_block = var.cidr
   }
-
+# Create subnet1 
   resource "aws_subnet" "mysubnet1" {
     vpc_id = aws_vpc.myvpc.id
     cidr_block = "10.0.2.0/24"
@@ -9,7 +10,7 @@ resource "aws_vpc" "myvpc" {
     map_public_ip_on_launch = true
     
   }
-
+# Create subnet2
   resource "aws_subnet" "mysubnet2" {
     vpc_id = aws_vpc.myvpc.id
     cidr_block = "10.0.1.0/24"
@@ -17,6 +18,7 @@ resource "aws_vpc" "myvpc" {
     map_public_ip_on_launch = true
   }
 
+#Create Interget gateway
   resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.myvpc.id
   }
@@ -39,10 +41,11 @@ resource "aws_route_table_association" "RTA2" {
   subnet_id      = aws_subnet.mysubnet2.id
   route_table_id = aws_route_table.RT.id
 }
+#Create security group
 resource "aws_security_group" "mysg" {
   description = "Allow TLS inbound traffic"
   vpc_id      = aws_vpc.myvpc.id
-
+# ingress = inbound rule
   ingress {
     description      = "HTTP"
     from_port        = 80
@@ -57,7 +60,7 @@ resource "aws_security_group" "mysg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-
+# egress = outbound rule
   egress {
     from_port        = 0
     to_port          = 0
@@ -65,7 +68,7 @@ resource "aws_security_group" "mysg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
   }
-
+# Create a S3 bucket and allow EC2 to access the S3 bucket via IAM role
   resource "aws_s3_bucket" "mys3" {
   bucket = "saifterraformfirstproject"
 
@@ -73,7 +76,7 @@ resource "aws_security_group" "mysg" {
     Name        = "My bucket"
   }
 }
-
+# Create a First EC2 Instance 
   resource "aws_instance" "webserver1" {
   ami          = "ami-0287a05f0ef0e9d9a"
   instance_type = "t2.micro"
@@ -86,6 +89,7 @@ resource "aws_security_group" "mysg" {
     Name = "HellopROject1"
   }
     }
+# Create a Second EC2 Instance
 resource "aws_instance" "webserver2" {
     ami = "ami-0287a05f0ef0e9d9a"
     instance_type = "t2.micro"
@@ -94,7 +98,7 @@ resource "aws_instance" "webserver2" {
     user_data = base64encode(file("userdata1.sh"))
   
 } 
-
+# Create a Application loadbalancer 
 resource "aws_lb" "webLB" {
     name = "MyfirstLB"
     internal = false
@@ -102,7 +106,7 @@ resource "aws_lb" "webLB" {
     security_groups = [aws_security_group.mysg.id]
     subnets = [aws_subnet.mysubnet1.id ,aws_subnet.mysubnet2.id]
   }
-
+# Create a Target group
   resource "aws_lb_target_group" "LBTG" {
     name = "MyfirstTG"
     port = 80
@@ -115,7 +119,7 @@ resource "aws_lb" "webLB" {
       port = "traffic-port"
     }
   }
-   
+# Target Group attach with each instances 
    resource "aws_lb_target_group_attachment" "attach1" {
     target_group_arn = aws_lb_target_group.LBTG.arn
     target_id = aws_instance.webserver1.id
@@ -127,7 +131,7 @@ resource "aws_lb" "webLB" {
     target_id = aws_instance.webserver2.id
     port = 80
      }
-
+# Create a loadbalancer lister to forward the traffic 
      resource "aws_lb_listener" "ALBI" {
         load_balancer_arn = aws_lb.webLB.arn
         port = 80
@@ -138,7 +142,7 @@ resource "aws_lb" "webLB" {
           target_group_arn = aws_lb_target_group.LBTG.arn
         }
        }
-
+# Output loadbalancer with the dns name 
     output "loadbalancerdns" {
         value = aws_lb.webLB.dns_name
     }
